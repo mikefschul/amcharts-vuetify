9178b54b2c379ce184d694b068b30476f00741e8
Mike Schulz <michael.schulz@vastech.co.za>
2020-11-18 10:47:03 +0200

    Removed models

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/Chriztiaan/amcharts-with-vuetify.git master:gh-pages
+git push -f https://github.com/mikefschulz/amcharts-vuetify.git master:gh-pages
 cd -
